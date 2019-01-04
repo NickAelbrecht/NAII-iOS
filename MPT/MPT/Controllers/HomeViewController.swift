09a98f64f47f9c7992de082a1ebc11174cfb3f0d
nick <nick.aelb@hotmail.com>
2019-01-04 15:23:39 +0100

    Als op de naam geklikt wordt, opent de app (instagram/facebook)

--- MPT/MPT/Controllers/HomeViewController.swift
+++ MPT/MPT/Controllers/HomeViewController.swift
@@ -10,5 +10,33 @@ import Foundation
 import UIKit
 
 class HomeViewController: UIViewController {
+    @IBOutlet weak var fbImage: UIImageView!
+    
+    
+    @IBOutlet weak var instaImage: UIImageView!
+    
+    
+    @IBAction func instaButtonKlik(_ sender: Any) {
+        let instagramHooks = "instagram://user?username=mpt_mergan_personal_training/"
+        let instagramUrl = NSURL(string: instagramHooks)
+        if UIApplication.shared.canOpenURL(instagramUrl! as URL) {
+            UIApplication.shared.open(instagramUrl! as URL, options: [:], completionHandler: nil)
+        } else {
+            //redirect to safari because the user doesn't have Instagram
+            UIApplication.shared.open(NSURL(string: "http://instagram.com/mpt_mergan_personal_training/")! as URL, options: [:], completionHandler: nil)
+        }
+    }
+    
+    
+    @IBAction func fbButtonKlik(_ sender: Any) {
+        let facebookHooks = "facebook://user?screen_name=Mergan.Personal.Training/"
+        let facebookUrl = NSURL(string: facebookHooks)
+        if UIApplication.shared.canOpenURL(facebookUrl! as URL) {
+            UIApplication.shared.open(facebookUrl! as URL, options: [:], completionHandler: nil)
+        } else {
+            //redirect to safari because the user doesn't have Facebook
+            UIApplication.shared.open(NSURL(string: "https://www.facebook.com/Mergan.Personal.Training/")! as URL, options: [:], completionHandler: nil)
+        }
+    }
     
 }
