9a4a10518c77637727ea03ac23d567cd54ef7a5a
nick <nick.aelb@hotmail.com>
2019-01-14 21:23:29 +0100

    Aanpassingen oefening toevoegen

---
+++ MPT/MPT/Controllers/OefeningToevoegenViewController.swift
@@ -0,0 +1,35 @@
+//
+//  OefeningToevoegenViewController.swift
+//  MPT
+//
+//  Created by Nick Aelbrecht on 14/01/2019.
+//  Copyright © 2019 Nick Aelbrecht. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class OefeningToevoegenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func cancelKnop(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
